dc4930089fed0f536c1b147eb77d5edce26c8d6d
Jeff Blankenburg <github@jeffblankenburg.com>
2013-05-29 21:00:23 -0400

    Initial commit

---
+++ Guess/CardPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Media;
+using System.IO.IsolatedStorage;
+
+namespace Guess
+{
+    public partial class CardPage : PhoneApplicationPage
+    {
+        
+        
+        public CardPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Guess/GameType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guess
+{
+    public class GameType
+    {
+        public string Name { get; set; }
+        public string ImageLocation { get; set; }
+        public string Description { get; set; }
+        public List<string> Cards { get; set; }
+        public int SortOrder { get; set; }
+    }
+}
